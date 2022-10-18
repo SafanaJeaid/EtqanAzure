f54c7dabadd6f123acf4b347e8a37699ab12de86
SafanaJeaid <safana.aljeaid@gmail.com>
2022-10-18 10:45:59 +0300

    export to excel

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using AbbyTryout.Data;
@@ -7,6 +8,8 @@ using AbbyTryout.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using ClosedXML.Excel;
 
 namespace AbbyTryout.Pages.Categories;
 
@@ -30,6 +33,53 @@ public class IndexModel : PageModel
     {
         // this is what retrieves all the items. instead of query.
         AlhasrTabel = _db.AlhasrTabel.FromSqlRaw("SELECT TOP (10) * FROM [dbo].[AlhasrTabel]");
+    }
+
+    /*    public async Task<IActionResult> OnPostExportExcelAsync()
+        {
+
+            var AlhasrTabelvar = _db.AlhasrTabel.FromSqlRaw("SELECT TOP (10) * FROM [dbo].[AlhasrTabel]");
+            // above code loads the data using LINQ with EF (query of table), you can substitute this with any data source.
+            var stream = new MemoryStream();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage(stream))
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+                workSheet.Cells.LoadFromCollection(AlhasrTabelvar, true);
+                package.Save();
+            }
+            stream.Position = 0;
+
+            string excelName = $"data-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+            // above I define the name of the file using the current datetime.
+
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName); // this will be the actual export.
+        }*/
+
+    public FileResult OnPostExport()
+    {
+        DataTable dt = new DataTable("Grid");
+        dt.Columns.AddRange(new DataColumn[2] { new DataColumn("location"),
+                                        new DataColumn("Title")});
+
+        var AlhasrTabelVar = from AlhasrTabel in this._db.AlhasrTabel.Take(10)
+                        select AlhasrTabel;
+
+        foreach (var customer in AlhasrTabelVar)
+        {
+            dt.Rows.Add(customer.location, customer.Title);
+        }
+
+        using (XLWorkbook wb = new XLWorkbook())
+        {
+            wb.Worksheets.Add(dt);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                wb.SaveAs(stream);
+                return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Grid.xlsx");
+            }
+        }
     }
 
     /*    public async Task OnGetAsync()
