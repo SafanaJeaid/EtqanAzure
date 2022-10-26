775a883edf4ce4c328efddd914c2a9fa2660913e
SafanaJeaid <safana.aljeaid@gmail.com>
2022-10-26 13:54:28 +0300

    adding MIW, and additions to main page.

--- Program.cs
+++ Program.cs
@@ -3,15 +3,31 @@ using AbbyTryout.Data;
 using AbbyTryout.Model;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization(options =>
+
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
 
 // Add services to the container.
 //builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 //using var connection = new SqlConnection("Server=tcp:restful.database.windows.net,1433;Database=WideWorldImportersUpdates;Persist Security Info=False;User ID=RestFulAPI;Password=S12345SH!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
@@ -35,8 +51,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
